bed7d9d217869833a7961eb1ec12d747805e7e5a
Egor Tyabin <pm53.tyabin@gmail.com>
2017-06-11 19:33:44 +0700

    Its finaly deleted?

--- Lab4_2s/ConsoleApplication11/tables.h
+++
@@ -1,9 +0,0 @@
-#ifndef TABLES_H
-#define TABLES_H
-struct FIO{ char F[20], I[20], O[20]; };
-struct ahuman{ char number[14], mark[30]; FIO name; };
-struct hesh { ahuman man; hesh *next; };
-void molodcy(hesh *&c, ahuman M);
-void create(hesh *&b, ahuman M);
-int key(ahuman T);
-#endif
